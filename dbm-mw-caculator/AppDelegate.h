c631cdf557f1b255b1fd775d2fc6ecd03a6ddc5b
Patrick Deng <motodexter@gmail.com>
2013-02-12 20:45:39 +0800

    Initial Commit

---
+++ dbm-mw-caculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dbm-mw-caculator
+//
+//  Created by Patrick Deng on 13-2-12.
+//  Copyright (c) 2013年 Code Animal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dbm-mw-caculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  dbm-mw-caculator
+//
+//  Created by Patrick Deng on 13-2-12.
+//  Copyright (c) 2013年 Code Animal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
