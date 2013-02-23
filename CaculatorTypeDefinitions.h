faed5d333f03dde00e7f4617d75ef7ef52a4feb9
Patrick Deng <motodexter@gmail.com>
2013-02-23 21:22:54 +0800

    Put all types definition in a standalone header file, and also put the header file into prefix.pch in project.

---
+++ CaculatorTypeDefinitions.h
@@ -0,0 +1,14 @@
+//
+//  CaculatorTypeDefinitions.h
+//  dbm-watt
+//
+//  Created by Patrick Deng on 13-2-23.
+//  Copyright (c) 2013年 Code Animal. All rights reserved.
+//
+
+#ifndef dbm_watt_CaculatorTypeDefinitions_h
+#define dbm_watt_CaculatorTypeDefinitions_h
+
+typedef enum {kW=1000000000, W=1000000, mW=1000, uW=1} WattUnit;
+
+#endif
