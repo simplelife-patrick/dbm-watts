9176b14bf18e852de0880413842663f01330eed9
simplelife-patrick <simplelife.patrick@gmail.com>
2013-08-16 19:53:02 +0800

    修正NavigationBar的显示问题

--- CaculatorViewController.m
+++ CaculatorViewController.m
@@ -373,8 +373,16 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    [self.navigationController setNavigationBarHidden:TRUE];
-    [super viewWillAppear:animated];
+	[super viewWillAppear:animated];
+    
+	[self.navigationController setNavigationBarHidden:YES animated:YES];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+    
+	[self.navigationController setNavigationBarHidden:NO animated:YES];
 }
 
 - (void)viewDidLoad
